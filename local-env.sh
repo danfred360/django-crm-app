379414abd02c6e0fc8c231b53be6c5b06886ef41
Daniel Frederick <dpfrederick@webstaurantstore.com>
2022-02-16 22:39:57 -0500

    finished auth

--- local-env.sh
+++ local-env.sh
@@ -6,7 +6,7 @@ export FLASK_DEBUG=1
 export SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 #alias p-run="source venv/bin/activate && python3 app.py"
-alias p-run="source venv/bin/activate && flask run"
+alias p-run="source venv/bin/activate && flask run project"
 
 alias p-build-image="docker image build -t maria ."
 alias p-run-image='docker run -p '"$PORT"':'"$PORT"' -d --name maria_app maria'
--- project/auth.py
+++ project/auth.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for, request
-from Werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user
 from .models import User
 from . import db
 
@@ -51,5 +51,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
